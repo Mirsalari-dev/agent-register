21954f9c14dd41173fa3c9eba8957a68f77d1c4b
AmirhosseinMirsalari <amir.tk7@gmail.com>
2024-04-10 12:58:54 -0500

    fix pwa verison

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Agent register</title>
         <meta name="description" content="Agent register for iranian poshesh company" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Typography variant="h2"  color="grey.900">سلام عرض شد</Typography>
